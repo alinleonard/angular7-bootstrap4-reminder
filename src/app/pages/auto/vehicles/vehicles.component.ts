1a92b0641a6599b06705ec49433577718dfa13b7
Alin <alin.busila@cerner.com>
2018-11-08 16:53:09 +0200

    fix lint

--- src/app/pages/auto/vehicles/vehicles.component.ts
+++ src/app/pages/auto/vehicles/vehicles.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'vehicles',
+  selector: 'ngx-auto-vehicles',
   template: `<div class="row">
     <ngx-auto-vehicles-list class="col-lg-6 col-md-6"></ngx-auto-vehicles-list>
     <ngx-auto-vehicles-add class="col-lg-6 col-md-6"></ngx-auto-vehicles-add>
