a214302831ca1ec82af14c087bfeeb3c5e38ca00
Alin <alin.busila@cerner.com>
2018-11-15 14:31:49 +0200

    created the minimal view for the history of auto activities. Would need to push to a custom component

--- src/app/pages/auto/dashboard/dashboard.component.ts
+++ src/app/pages/auto/dashboard/dashboard.component.ts
@@ -7,13 +7,25 @@ import { HistoryService } from '../../components/history-tree/history.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class AutoDashboardComponent implements OnInit {
-  public history: HistoryService;
+  // public history: HistoryService;
+
+  history: any[];
 
   constructor(history: HistoryService) {
-    this.history = history;
+    // this.history = history;
   }
 
   ngOnInit() {
+    this.history = [
+      { name: 'Fine',  date: '03/20/2018', timeleft: '15 days left', icon: 'fa-credit-card'},
+      { title: 'March 2019'},
+      { name: 'Battery', odometer: '140000 km', date: '11/13/2018', timeleft: 'Today', icon: 'fa-car'},
+      { title: 'November 2018'},
+      { name: 'Brake Fluid', location: 'Brasov Country', odometer: '200000 km', date: '14/13/2018', price: '$2.000', icon: 'fa-wrench'},
+      { name: 'Spark Plugs', location: 'Brasov Country', odometer: '200000 km', date: '14/13/2018', price: '$6.000', icon: 'fa-wrench'},
+      { title: 'October 2018'},
+      { name: 'Brake Fluid', location: 'Brasov Country', odometer: '200000 km', date: '14/13/2018', price: '$2.000', icon: 'fa-wrench'}
+    ]
   }
 
 }
